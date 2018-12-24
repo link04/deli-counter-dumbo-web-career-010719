075066497755c404cc371a37acc5e152a57b93fd
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 22:25:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ def line (array)
   if array.size == 0 
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
+  
     array.each do |costumer|
       puts "#{costumer}"
     end
