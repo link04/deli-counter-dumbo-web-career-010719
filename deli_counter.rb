3c96beeed56819914b9518a29c99179254215a75
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 22:58:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(array)
   if array.size() == 0 
-    puts "The line is currently empty."
+    puts "There is nobody waiting to be served!"
   else
     puts "Now serving #{array.shift}."
   end
