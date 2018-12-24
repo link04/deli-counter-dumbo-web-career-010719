9347af6a4370b228704b6b19754d8bcd3536da4c
Maximo Bautista <max.bautista97@gmail.com>
2018-12-24 22:21:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 def line (array)
   if array.size == 0 
-    puts "The line is empty."
+    puts "The line is currently empty."
   else
     puts "The line is currently:"
     array.each do |costumer|
